7062d552f83b4c25279f27de899b03f58847aa1a
Sechaba Mothobi <Lefatshe70@yahoo.com>
2016-04-10 18:05:48 +0200

    commit init "Time as TimeShares"

---
+++ app/models/article.server.model.js
@@ -0,0 +1,112 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Load the module dependencies
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+// Define a new 'ArticleSchema'
+var ArticleSchema = new Schema({
+	  // Timeshare Discriptions
+      created: {
+        type: Date,
+        default: Date.now
+      },
+      title: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Title cannot be blank'
+      }, 
+      category: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Category cannot be blank'
+      }, 
+      province: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'province cannot be blank'
+      }, 
+      discription: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Discription cannot be blank'
+      }, 
+      creator: {
+        type: Schema.ObjectId,
+        ref: 'User' 
+      },
+
+      // Timeshare Images
+      images: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Images cannot be blank'
+      }, 
+
+      // Timeshare attractions
+      attractions: {
+        type: String,
+        default: '',
+        trim: true
+      }, 
+      nearby: {
+        type: String,
+        default: '',
+        trim: true
+      },
+
+      // Timeshare purchase price & discription
+      purchase: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Rent or buy cannot be blank'
+      },
+      price: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Price or buy cannot be blank'
+      },
+      bedrooms: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'bedrooms cannot be blank'
+      },
+      datemin: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Date stay cannot be blank'
+      },
+      datemax: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Date stay cannot be blank'
+      },
+
+      // Admin options
+      availability: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Availability cannot be blank'
+      },
+      verify: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Availability cannot be blank'
+      }
+});
+
+// Create the 'Article' model out of the 'ArticleSchema'
+mongoose.model('Article', ArticleSchema);
---
+++ public/articles/controllers/articles.client.controller.js
@@ -0,0 +1,110 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// Create the 'articles' controller
+angular.module('articles').controller('ArticlesController', ['$scope', '$routeParams', '$location', 'Authentication', 'Articles',
+    function($scope, $routeParams, $location, Authentication, Articles) {
+    	// Expose the Authentication service
+        $scope.authentication = Authentication;
+
+        // Create a new controller method for creating new articles
+        $scope.create = function() {
+        	// Use the form fields to create a new article $resource object
+            var article = new Articles({
+                title: this.title,
+                category: this.category,
+                province: this.province,
+                discription: this.discription,
+
+                // images
+                images: this.images,
+
+                // attractions
+                attractions: this.attractions,
+                nearby: this.nearby,
+
+                // purchase price & discription
+                purchase: this.purchase,
+                price: this.price,
+                bedrooms: this.bedrooms,
+                datemin: this.datemin,
+                datemax: this.datemax,
+
+                // action timeshare
+                availability: this.availability,
+                verify: this.verify
+            });
+
+
+
+
+
+
+
+            
+
+
+
+
+
+
+
+
+            // Use the article '$save' method to send an appropriate POST request
+            article.$save(function(response) {
+            	// If an article was created successfully, redirect the user to the article's page 
+                $location.path('articles/' + response._id);
+            }, function(errorResponse) {
+            	// Otherwise, present the user with the error message
+                $scope.error = errorResponse.data.message;
+            });
+        };
+
+        // Create a new controller method for retrieving a list of articles
+        $scope.find = function() {
+        	// Use the article 'query' method to send an appropriate GET request
+            $scope.articles = Articles.query();
+        };
+
+        // Create a new controller method for retrieving a single article
+        $scope.findOne = function() {
+        	// Use the article 'get' method to send an appropriate GET request
+            $scope.article = Articles.get({
+                articleId: $routeParams.articleId
+            });
+        };
+
+        // Create a new controller method for updating a single article
+        $scope.update = function() {
+        	// Use the article '$update' method to send an appropriate PUT request
+            $scope.article.$update(function() {
+            	// If an article was updated successfully, redirect the user to the article's page 
+                $location.path('articles/' + $scope.article._id);
+            }, function(errorResponse) {
+            	// Otherwise, present the user with the error message
+                $scope.error = errorResponse.data.message;
+            });
+        };
+
+        // Create a new controller method for deleting a single article
+        $scope.delete = function(article) {
+        	// If an article was sent to the method, delete it
+            if (article) {
+            	// Use the article '$remove' method to delete the article
+                article.$remove(function() {
+                	// Remove the article from the articles list
+                    for (var i in $scope.articles) {
+                        if ($scope.articles[i] === article) {
+                            $scope.articles.splice(i, 1);
+                        }
+                    }
+                });
+            } else {
+            	// Otherwise, use the article '$remove' method to delete the article
+                $scope.article.$remove(function() {
+                    $location.path('articles');
+                });
+            }
+        };
+    }
+]);
