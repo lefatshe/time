72094161af78e13e2c1300975fd7917652d0b8fe
Sechaba Mothobi <Lefatshe70@yahoo.com>
2016-04-15 20:49:35 +0200

    nav routes schema

---
+++ public/navbar/config/navbar.client.routes.js
@@ -0,0 +1,16 @@
+// Invoke 'strict' JavaScript mode
+'use strict';
+
+// // Configure the 'example' module routes
+// angular.module('navbar').config(['$routeProvider',
+// 	function($routeProvider) {
+// 		$routeProvider.
+// 		when('//#!helloAdmin', {
+// 			controller: navbar,
+//         	templateUrl: 'page1.htm'
+// 		}).
+// 		otherwise({
+// 			redirectTo: '/'
+// 		});
+// 	}
+// ]); 
---
+++ public/navbar/controllers/navbar.client.controller.js
@@ -0,0 +1,29 @@
+// Create the 'example' controller
+angular.module('navbar').controller('NavCtrl', ['$scope', '$location',
+    function($scope, $location) {
+        $scope.states = {};
+        $scope.states.activeItem = 'item1';
+
+        $scope.items = [{
+            id: 'item1',
+            title: 'Timeshare',
+            icon: 'images/minutes.svg',
+            clas: 'timeshare',
+            path: '/#!helloAdmin/'
+        }, {
+            id: 'item2',
+            title: 'Site Posts',
+            icon: 'images/letter.svg',
+            clas: 'posts',
+            path: '/site-posts'
+        }, {
+            id: 'item3',
+            title: 'Upload Media',
+            icon: 'images/up-arrow.svg',
+            clas: 'media',
+            path: '/media'
+        }];
+        
+    }
+]);
+
